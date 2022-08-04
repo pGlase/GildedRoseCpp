95750cc2ea519c71a1b3196dc9eae90bd99313c4
Pascal Glase <killerkarnickel93@web.de>
2022-08-04 23:53:40 +0200

    first tests added

--- test/gildedrose_catch.cpp
+++ test/gildedrose_catch.cpp
@@ -2,6 +2,7 @@
 #include "ApprovalTests.hpp"
 
 #include "GildedRose.h"
+#include <algorithm>
 
 std::ostream& operator<<(std::ostream& os, const Item& obj)
 {
@@ -11,11 +12,86 @@ std::ostream& operator<<(std::ostream& os, const Item& obj)
             << ", quality: " << obj.quality;
 }
 
-TEST_CASE("UpdateQuality") {
+void AssertItemUpdated(const Item& beforeUpdate, const Item& toTest)
+{
+    REQUIRE(toTest.sellIn == (beforeUpdate.sellIn-1));
+    REQUIRE(toTest.quality == (beforeUpdate.quality-1));
+}
+
+TEST_CASE("UpdateQuality_SingleItem") {
 
     vector<Item> items;
-    items.push_back(Item("foo", 0, 0));
+    Item testA("foo", 1, 1);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+
+    AssertItemUpdated(testA, items.front());
+}
+
+TEST_CASE("UpdateQuality_SingleItem_QualityLossTwice_AfterSelltime") {
+
+    vector<Item> items;
+    Item testA("foo", 0, 10);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 8);
+    app.updateQuality();
+    REQUIRE(items.front().quality == 6);
+}
+
+TEST_CASE("UpdateQuality_SingleItem_UpdateZeroQuality") {
+
+    vector<Item> items;
+    Item testA("foo", 0, 0);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+
+    REQUIRE(items.front().quality >= 0);
+}
+
+TEST_CASE("UpdateQuality_AgedBree_IncreaseInQuality") {
+
+    vector<Item> items;
+    Item testA("Aged Brie", 10, 10);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 11);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 12);
+}
+
+TEST_CASE("UpdateQuality_AgedBree_IncreaseInQuality_NotGreaterThan50") {
+
+    vector<Item> items;
+    Item testA("Aged Brie", 10, 50);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 50);
+}
+
+TEST_CASE("ItemsInSystem_QualityClampedAt50_Assumption", "[.]") {
+
+    vector<Item> items;
+    Item testA("foo", 10, 100);
+    items.push_back(testA);
+
     GildedRose app(items);
     app.updateQuality();
-    REQUIRE("fixme" == app.items[0].name);
+    REQUIRE(items.front().quality == 50);
 }
