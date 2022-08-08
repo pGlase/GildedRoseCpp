5985befbc88df2252688f0ccf80d9b69fea732dd
Pascal Glase <killerkarnickel93@web.de>
2022-08-08 22:20:31 +0200

    Tests written

--- test/gildedrose_catch.cpp
+++ test/gildedrose_catch.cpp
@@ -85,6 +85,104 @@ TEST_CASE("UpdateQuality_AgedBree_IncreaseInQuality_NotGreaterThan50") {
     REQUIRE(items.front().quality == 50);
 }
 
+TEST_CASE("UpdateQuality_Sulfuras_NeverChangesValue") {
+
+    vector<Item> items;
+    Item sulfuras("Sulfuras, Hand of Ragnaros", 1, 50);
+    items.push_back(sulfuras);
+
+    GildedRose app(items);
+
+    for(int i=0; i < 10; i++)
+    {
+        app.updateQuality();
+    }
+    REQUIRE(items.front().quality > 0);
+}
+
+TEST_CASE("UpdateQuality_Sulfuras_NeverChangesSelldate") {
+
+    vector<Item> items;
+    Item sulfuras("Sulfuras, Hand of Ragnaros", 1, 50);
+    items.push_back(sulfuras);
+
+    GildedRose app(items);
+
+    for(int i=0; i < 10; i++)
+    {
+        app.updateQuality();
+    }
+    REQUIRE(items.front().sellIn == sulfuras.sellIn);
+}
+
+TEST_CASE("UpdateQuality_BackstagePass_IncreaseInQuality") {
+
+    vector<Item> items;
+    Item testA("Backstage passes to a TAFKAL80ETC concert", 20, 10);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 11);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 12);
+}
+
+TEST_CASE("UpdateQuality_BackstagePass_IncreaseInQuality_10dayOrLess") {
+
+    vector<Item> items;
+    Item testA("Backstage passes to a TAFKAL80ETC concert", 10, 10);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 12);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 14);
+}
+
+TEST_CASE("UpdateQuality_BackstagePass_IncreaseInQuality_5dayOrLess") {
+
+    vector<Item> items;
+    Item testA("Backstage passes to a TAFKAL80ETC concert", 5, 10);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 13);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 16);
+        app.updateQuality();
+    REQUIRE(items.front().quality == 19);
+}
+
+TEST_CASE("UpdateQuality_BackstagePass_IncreaseInQuality_ZeroValueAtExpiration") {
+
+    vector<Item> items;
+    Item testA("Backstage passes to a TAFKAL80ETC concert", 3, 10);
+    items.push_back(testA);
+
+    GildedRose app(items);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 13);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 16);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 19);
+
+    app.updateQuality();
+    REQUIRE(items.front().quality == 0);
+}
+
 TEST_CASE("ItemsInSystem_QualityClampedAt50_Assumption", "[.]") {
 
     vector<Item> items;
